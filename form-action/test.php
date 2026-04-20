450a84238fdadc47955d2e11980122c50f54d771
j.lee <j.lee@core-tech.jp>
2026-04-20 13:40:22 +0900

    formのテスト

---
+++ form-action/test.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        
+    <form action="action.php" method="post">
+
+    <label for="name">名前:</label>
+    <input name="name" id="name" type="text">
+
+    <button type="submit">送信</button>
+</form>
+    </body>
+</html>
